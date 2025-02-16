b26d8c09a3da4df42cf05fe352fc5b49e47c8e83
Ryan7mg <2786978M@student.gla.ac.uk>
2025-02-16 22:15:49 +0000

    refactor(content): integrate new crawlers

--- src/content/send_content.js
+++ src/content/send_content.js
@@ -1,170 +1,106 @@
-const TEN_SECONDS = 10000;
-const SIXTY_SECONDS = 60000;
-
-const currentURL = window.location.href;
-
-const quipRegex = /^https?:\/\/(?:www\.)?quip-amazon\.com(?:\/|$)/;
-
-function checkSitesList() {
-  return new Promise((resolve) => {
-    chrome.storage.local.get(['allowedSites'], (result) => {
-      const storedSiteList = result.allowedSites;
-      const sitesList = storedSiteList || [];
-      const currentHostname = window.location.hostname;
-      resolve(sitesList.includes(currentHostname));
-    });
-  });
-}
-
-function checkUrlsList() {
-  return new Promise((resolve) => {
-    chrome.storage.local.get(['allowedURLs'], (result) => {
-      const storedUrlsList = result.allowedURLs;
-      const urlsList = storedUrlsList || [];
-      resolve(urlsList.includes(currentURL));
-    });
-  });
-}
-
-function checkStringMatchesList() {
-  return new Promise((resolve) => {
-    chrome.storage.local.get(['allowedStringMatches'], (result) => {
-      const storedMatchesList = result.allowedStringMatches;
-      const matchesList = storedMatchesList || [];
-      resolve(matchesList.some((match) => currentURL.indexOf(match) > -1));
-    });
-  });
-}
-
-function checkRegexList() {
-  return new Promise((resolve) => {
-    chrome.storage.local.get(['allowedRegex'], (result) => {
-      const storedRegexList = result.allowedRegex;
-      const regexList = storedRegexList || [];
-
-      const isMatch = regexList.some((regex) => new RegExp(regex).test(currentURL));
-      resolve(isMatch);
-    });
-  });
-}
-
-// sometimes params contain invalid characters like underscores
-// encode them to avoid the URL constructor throwing an error
-const parseURLWithParams = function parseURLWithParams(parts) {
-  const baseURL = parts[0];
-  const queryParams = parts[1];
-  const params = queryParams.split('&');
-  let newURL = `${baseURL}?`;
-  params.forEach((param, index) => {
-    if (index > 0) {
-      newURL += '&';
-    }
-
-    const [key, value] = param.split('=');
-    const encodedKey = encodeURIComponent(key);
-    const encodedValue = encodeURIComponent(value);
-    newURL += `${encodedKey}=${encodedValue}`;
-  });
-
-  return newURL;
-};
-
-function callIndexer(url, clicked) {
-  const visibleTextContent = document.body.innerText;
-  const { title } = document;
-  chrome.runtime.sendMessage({
-    action: 'sendVisibleTextContent',
-    visibleTextContent,
-    url,
-    title,
-    clicked,
-  });
-}
-
-const indexQuip = function indexQuip() {
-  if (quipRegex.test(currentURL)) {
-    try {
-      chrome.storage.local.get(['indexed']).then((result) => {
-        const indexed = result.indexed || {};
-        if (Object.keys(indexed).length > 0) {
-          indexed.links = new Set(indexed.links);
-          if (indexed.links.has(currentURL)) {
-            for (let i = 0; i < indexed.corpus.length; i += 1) {
-              const page = indexed.corpus[i];
-              if (page.url === currentURL) {
-                const documentEditor = document.getElementsByClassName('document-editor');
-                if (documentEditor.length === 1) {
-                  const quipContent = documentEditor[0].innerText;
-                  if (page.body !== quipContent) {
-                    page.body = quipContent;
-                    indexed.links = Array.from(indexed.links);
-                    chrome.storage.local.set({ indexed });
-                    chrome.runtime.sendMessage({ action: 'updateIndexing' });
-                    return;
-                  }
-                }
-              }
-            }
-          }
-        }
-
-        const documentEditor = document.getElementsByClassName('document-editor');
-        if (documentEditor.length >= 1) {
-          callIndexer(currentURL, false);
-        }
-      });
-    } catch (error) {
-      // extension will have been reloaded, ignore
-    }
+class CrawledDocument {
+  constructor(id, allText, title) {
+      this.id = id;
+      this.allText = allText;
+      this.title = title;
   }
-};
 
-$(document).ready(() => {
-  Promise.all([
-    checkSitesList(),
-    checkUrlsList(),
-    checkStringMatchesList(),
-    checkRegexList(),
-  ]).then((results) => {
-    if (results.some((result) => result)) {
-      $(document).on('click', 'a', (event) => {
-        let link = $(event.target).prop('href');
-        if (!link) return;
-        const parts = link.split('?');
-        if (parts.length > 1) {
-          link = parseURLWithParams(parts);
-        }
-        const clickedURL = new URL(link);
-        const clickedURLPath = clickedURL.pathname.replace(/\/[^\/]+$/, '');
-        const currentURLPath = new URL(currentURL).pathname.replace(
-          /\/[^\/]+$/,
-          '',
-        );
+  getId() {
+      return this.id;
+  }
+  
+  getAllText() {
+      return this.allText;
+  }
 
-        // Check if the clicked link is not an anchor link within the same page
-        if (
-          clickedURL.origin === new URL(currentURL).origin
-          && clickedURLPath === currentURLPath
-        ) {
-          // Send a message indicating that the page has navigated
-          try {
-            callIndexer(clickedURL, true);
-          } catch (error) {
-            // extension will have been reloaded, ignore
-          }
-        }
-      });
-      if (quipRegex.test(currentURL)) {
-        (async () => {
-          await new Promise((resolve) => {
-            setTimeout(resolve, TEN_SECONDS);
-          });
-          indexQuip();
-          setInterval(indexQuip, SIXTY_SECONDS);
-        })();
-      } else {
-        callIndexer(currentURL, false);
-      }
+  getTitle() {
+      return this.title;
+  }
+}
+
+class QuipCrawler {
+    
+     getApplicableDomains() {
+        return ["quip-amazon"];    
     }
-  });
-});
+
+     process(url, document) {
+        const allWords = document.body.innerText;
+        const normalizedWords = allWords.replace(/(\r\n|\n|\r)/gm, " ");
+        let index = url.lastIndexOf('#');
+        let normalizedUrl = url;
+        if(index !== -1){
+            normalizedUrl = url.substring(0,index);
+        }
+        return new CrawledDocument(normalizedUrl, normalizedWords, document.title);
+    }
+
+     isCompatible(url){
+        let domainURL = (new URL(url));
+        let domain = domainURL.hostname.toLocaleLowerCase();
+        let isMatch = false;
+        let compatibleDomains = this.getApplicableDomains();
+        for(let i = 0; i < compatibleDomains.length; i++){
+            if(domain.indexOf(compatibleDomains[i]) !== -1){
+                isMatch = true;
+            }
+        }
+        return isMatch;
+    } 
+}
+
+class GenericCrawler {
+
+
+     getApplicableDomains() {
+        return ["amazon","a2z"];
+    }
+     process(url, document) {
+        const allWords = document.body.innerText;
+        const normalizedWords = allWords.replace("\n"," ");
+        return new CrawledDocument(url, normalizedWords, document.title);
+    }
+
+     isCompatible(url){
+        let domainURL = (new URL(url));
+        let domain = domainURL.hostname.toLocaleLowerCase();
+        let isMatch = false;
+        let compatibleDomains = this.getApplicableDomains();
+        for(let i = 0; i < compatibleDomains.length; i++){
+            if(domain.indexOf(compatibleDomains[i]) !== -1){
+                isMatch = true;
+            }
+        }
+        return isMatch;
+    } 
+}
+
+
+
+console.log("CONTENT SCRIPT: Hello World");
+
+const crawlers =[new QuipCrawler(), new GenericCrawler()];
+
+const indexDocument = (doc )=>{
+    console.log("sending.....");
+    chrome.runtime.sendMessage({document: doc }, function(response) {
+    });
+}
+
+const scrapePage = () => {
+    if (document.readyState !== "complete") {
+        return;
+    }
+    
+    const url = window.location.href;
+    for (const crawler of crawlers) {
+        if(crawler.isCompatible(url)){
+            let crawledDocument = crawler.process(url, document);
+            indexDocument(crawledDocument);
+            console.log("Document Crawled: "+ crawledDocument.getId());
+            break;
+        }
+    }
+}
+
+setInterval(scrapePage,15000);
